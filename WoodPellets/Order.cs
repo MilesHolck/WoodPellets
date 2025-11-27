e705d14456415017e4f9adf2598586e776a7c44b
Ida Emilie Balch Hansen <idh004@edu.zealand.dk>
2025-11-27 12:51:41 +0100

    Opretter Order

---
+++ WoodPellets/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoodPellets
+{
+    internal class Order
+    {
+
+        private static int _nextId; 
+        
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public DateOnly OrderDate { get; set; } 
+        public DateOnly DeliveryDate { get; set; }
+
+        
+
+    }
+}
